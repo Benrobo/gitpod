0d12ab8d5d9bb0eb00db4afc99b18983f7579b68
benrobo <alumonabenaiah71@gmail.com>
2022-01-01 21:42:01 +0100

    updated utils

--- gitpod.js
+++ gitpod.js
@@ -1,34 +1,40 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
 
-const { program } = require("commander");
+const chalk = require("chalk")
+const { Command } = require("commander");
+
+const program = new Command()
+
 const v = "0.0.1";
+
 program.version(v);
 
 // log
 const log = console.log
 
-const { genHash, authQuestions, initGitpod, ANCII } = require("./lib/utils/index");
+const util = require("./lib/utils/index");
 
-log(ANCII("gitpod"));
+log(util.ANCII("gitpod"));
 
 program
   .option("-v, --version", "extra information for gitpod")
   .option("-l, --login", "log in into gitpod using github acct")
-  .option("-i, --init", "initialize gitpod");
+  .option("-i, --init", "initialize gitpod")
+  .parse(process.argv)
 
-program.parse(process.argv);
 
 const opt = program.opts();
 
 
-// if (options.v || options.version) {
-//   return console.log("chalk.green(version)");
-// } else if (options.i || options.init) {
-//   async function run(){
-//     return await initGitpod();
-//   }
-//   run()
-// }
-// else{
-//     chalk.red("command is invalid")
-// }
+if (opt.v || opt.version) {
+    log("")
+    return log("version number")
+} else if (opt.i || opt.init) {
+  async function run(){
+    return await util.initGitpod();
+  }
+  run()
+}
+else{
+    chalk.red("command is invalid")
+}
